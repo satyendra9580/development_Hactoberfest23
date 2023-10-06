ca90e01b1e3b8e0277145f760e33c9316cee315a
Satyendra singh <121458324+satyendra9580@users.noreply.github.com>
2023-10-07 02:11:57 +0530

    Create bubble.c

---
+++ bubble.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int temp;
+    int swapped;
+
+    for (int i = 0; i < n - 1; i++) {
+        swapped = 0; // Initialize the swapped flag to 0
+
+        // Inner loop to perform comparisons and swaps
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+
+                swapped = 1; // Set swapped flag to 1
+            }
+        }
+
+        // If no two elements were swapped in the inner loop,
+        // the array is already sorted, and we can break out of the outer loop
+        if (swapped == 0) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
